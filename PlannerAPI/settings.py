5110b02eff57f4f15fb519c9832ae5d49f8c9e26
santiago <gerencia@evolutionsoluciones.com>
2019-10-26 13:41:31 -0500

    agregado diagrama

--- PlannerAPI/settings.py
+++ PlannerAPI/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'planner.apps.PlannerConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
