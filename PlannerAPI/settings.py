2d2ae5efcc3d4b6974f0b17bc1b8264ebd3077a7
Cordova9 <charliecrdv@gmail.com>
2019-11-20 21:35:41 -0500

    Revert "m"
    This reverts commit 02bb4a97fef042d7a3ac483352752268a1e1b596, reversing
    changes made to 401387747ae67f5b020de98ea13ac6372020d56f.

--- PlannerAPI/settings.py
+++ PlannerAPI/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'planner.apps.PlannerConfig',
-    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
